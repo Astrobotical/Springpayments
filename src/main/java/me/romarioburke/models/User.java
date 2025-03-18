0435434fbcad1334f18db3c010664de6b8637b0a
Romario Burke <romari123456789@gmail.com>
2025-03-18 12:52:24 -0500

    #Added: The User Entity/Model

--- src/main/java/me/romarioburke/models/User.java
+++ src/main/java/me/romarioburke/models/User.java
@@ -2,20 +2,25 @@ package me.romarioburke.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import me.romarioburke.models.Orders.Order;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(name = "Orders")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long uid;
+    private Long UID;
 
     @Column(nullable = false)
     private String firstname;
 
     @Column(nullable = false)
     private String lastname;
+
     @Column(nullable = false)
     private String password;
 
@@ -24,4 +29,7 @@ public class User {
 
     @Column(nullable = true)
     private boolean isVerified;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders = new ArrayList<>();
 }
