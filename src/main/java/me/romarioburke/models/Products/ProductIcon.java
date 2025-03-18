006d19954c92b4560a1f4e955d0f6526be68feac
Romario Burke <romari123456789@gmail.com>
2025-03-18 13:07:48 -0500

    #Added: The ProductsIcon Entity/Model

---
+++ src/main/java/me/romarioburke/models/Products/ProductIcon.java
@@ -0,0 +1,21 @@
+package me.romarioburke.models.Products;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "ProductIcons")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class ProductIcon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long iconID;
+
+    @Column(nullable = false)
+    private String iconPath;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "productID", nullable = false)
+    private Products product;
+}
