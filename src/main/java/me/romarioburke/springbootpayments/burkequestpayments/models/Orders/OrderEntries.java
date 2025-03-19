eca4162db9b0807f1351b9372667e5eaf1b58394
Romario Burke <romari123456789@gmail.com>
2025-03-18 21:27:12 -0500

    #Fixed: Moved the Order entries to the main folder.

---
+++ src/main/java/me/romarioburke/springbootpayments/burkequestpayments/models/Orders/OrderEntries.java
@@ -0,0 +1,21 @@
+package me.romarioburke.springbootpayments.burkequestpayments.models.Orders;
+
+
+import jakarta.persistence.*;
+import me.romarioburke.springbootpayments.burkequestpayments.models.Products.Products;
+
+
+@Entity
+@Table(name = "OrderEntries")
+public class OrderEntries {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long EntryID;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "ProductID", nullable = false)
+    private Products product;
+    @ManyToOne
+    @JoinColumn(name = "order_orderID", nullable = false)
+    private Order order;
+}
