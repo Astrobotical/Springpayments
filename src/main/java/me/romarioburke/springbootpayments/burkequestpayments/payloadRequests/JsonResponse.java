00f16b3799fbcb4513da90970e9765ef58f08a5c
Romario Burke <romari123456789@gmail.com>
2025-03-19 23:45:05 -0500

    #Added: JsonResponse.java

---
+++ src/main/java/me/romarioburke/springbootpayments/burkequestpayments/payloadRequests/JsonResponse.java
@@ -0,0 +1,37 @@
+package me.romarioburke.springbootpayments.burkequestpayments.payloadRequests;
+
+public class JsonResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public JsonResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
