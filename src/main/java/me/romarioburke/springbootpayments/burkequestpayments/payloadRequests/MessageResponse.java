ccd29c04007bba1a153f0475da070468e40091b1
Romario Burke <romari123456789@gmail.com>
2025-03-19 23:44:37 -0500

    #Added: MessageResponse.java

---
+++ src/main/java/me/romarioburke/springbootpayments/burkequestpayments/payloadRequests/MessageResponse.java
@@ -0,0 +1,15 @@
+package me.romarioburke.springbootpayments.burkequestpayments.payloadRequests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageResponse {
+        private String message;
+
+        public MessageResponse(String message) {
+            this.message = message;
+        }
+
+}
